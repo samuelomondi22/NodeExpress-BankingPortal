aba1d30674677494ab5f96e2ca23b55c6d50aa4e
samuelomondi22 <omondisam696@gmail.com>
2019-05-15 15:21:36 +0300

    completed the module

--- README.md
+++ README.md
@@ -1,3 +1,33 @@
 # Build a Banking Portal with Node & Express
 
 In this project you’ll build a simple Banking Portal with Node & Express. There will be seven different routes that will include index, profile, savings, checking, credit, transfer and payment. We'll provide you with some starter code that includes several view templates. You'll use Node's built-in `fs` library to read and write data. We will also use the Express router to make the application more modular.
+
+
+
+#app.set( name , value )
+name is assigned to value
+app.set(name,value)
+app.get(value) //name
+
+#middleware
+software that acts as a bridge between an operating system or database and applications.
+takes results of one function and passes it on to the next
+Essentially functioning as hidden translation layer, middleware enables communication and data management for distributed applications. It is sometimes called plumbing, as it connects two applications together so data and databases can be easily passed between the “pipe"
+
+#common middleware examples
+#database middleware
+Database middleware is a generic term used to refer to software infrastructure that supports exchange of info btwn software components
+#application server middleware
+complex transaction between applications
+#message oriented middleware
+Message-oriented middleware (MOM) is software or hardware infrastructure supporting sending and receiving messages between distributed systems. MOM allows application modules to be distributed over heterogeneous( diverse in character or content ) platforms and reduces the complexity of developing applications that span multiple operating systems and network protocols. 
+#web middleware
+Web services middleware works like a client-server architecture where the Web services application is the client and the middleware is the server, that is, it provides services to the client.
+
+#Middleware functions can perform the following tasks:
+Execute any code.
+Make changes to the request and the response objects.
+End the request-response cycle.
+Call the next middleware in the stack.
+
+if it doesnt call the next() the req willl be left hanging
--- src/app.js
+++ src/app.js
@@ -6,14 +6,25 @@ const express = require('express');
 
 const app = express();
 //where our views can be found
+//shows that what we rendering is from views
 app.set('views',path.join(__dirname, 'views'));
+//sets all my templates(view engines ) to be on '.ejs'
 app.set('view engine', 'ejs');
-//tell express how to find our static directory nad how to serve those files 
+//tell express how to find our static directory and how to serve those files 
+//express.static() is a bit of middleware that maps directory names to the path directory for easy lookup. Usually you'll have
+//Static files are best considered as files that are not include by something like NPM or Bower. They are your own scripts, stylesheets, images, etc
+//Serve static content for the app from the “public” directory in the application directory
 app.use(express.static(path.join(__dirname,'public')));
 
+app.use(express.urlencoded({ extended: true }));
+
+//readFileSync() is synchronous and blocks execution until finished. doesnt take a call back
+//readFile() are asynchronous and return immediately while they function in the background. takes a callback(function argument)
 const accountData = fs.readFileSync(
     path.join(__dirname, 'json', 'accounts.json'), 'utf8'
 );
+//parse takes string and returns as a javascript object
+//The JSON.parse() method parses a JSON string, constructing the JavaScript value or object described by the string
 const accounts = JSON.parse(accountData);
 
 const userData = fs.readFileSync(
@@ -22,6 +33,7 @@ const userData = fs.readFileSync(
 const users = JSON.parse(userData);
 
 //render
+//http GET retrieves info from server
 app.get('/', (req, res ) => {
     res.render('index', {title: 'Account Summary', accounts })
 });
@@ -36,6 +48,23 @@ app.get('/checking', (req, res) => {
 
 app.get('/credit', (req, res) => {
     res.render('account', { account: accounts.credit })
+})
+
+app.get('/transfer', (req, res ) => res.render('transfer'));
+app.post('/transfer', (req, res) =>{
+    accounts[req.body.from].balance = accounts[req.body.from].balance - req.body.amount
+    accounts[req.body.to].balance = parseInt(accounts[req.body.to].balance) + parseInt(req.body.amount, 10)
+    const accountsJSON = JSON.stringify(accounts, null, 4);
+    fs.writeFileSync(path.join(__dirname, 'json/accounts.json'), accountsJSON, 'utf8')
+    res.render('transfer', {message: 'Transfer completed'})
+})
+app.get('/payment', (req, res ) => res.render('payment', { account: accounts.credit }))
+app.post('/payment', (req, res ) =>{
+    accounts.credit.balance -= req.body.amount;
+    accounts.credit.available += parseInt(req.body.amount, 10)
+    const accountsJSON = JSON.stringify(accounts, null ,4)
+    fs,writeFileSync(path.join(__dirname, 'json', 'acccounts.json'), accountsJSON, 'utf8')
+    res.render('payment', { message: 'payment successful', account: accounts.credit })
 })
 
 app.get('/profile', (req, res) =>{
