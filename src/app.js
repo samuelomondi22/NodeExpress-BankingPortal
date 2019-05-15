34d572a3704371b5a2ed22692c52cee1b8c2aae2
samuelomondi22 <omondisam696@gmail.com>
2019-05-15 15:35:37 +0300

    completed the module

--- src/app.js
+++ src/app.js
@@ -58,13 +58,14 @@ app.post('/transfer', (req, res) =>{
     fs.writeFileSync(path.join(__dirname, 'json/accounts.json'), accountsJSON, 'utf8')
     res.render('transfer', {message: 'Transfer Completed'})
 })
-app.get('/payment', (req, res ) => res.render('payment', { account: accounts.credit }))
-app.post('/payment', (req, res ) =>{
+
+app.get('/payment', (req, res ) => res.render('payment', { account: accounts.credit }));
+app.post('/payment', (req, res ) => {
     accounts.credit.balance -= req.body.amount;
-    accounts.credit.available += parseInt(req.body.amount, 10)
-    const accountsJSON = JSON.stringify(accounts, null ,4)
-    fs,writeFileSync(path.join(__dirname, 'json', 'acccounts.json'), accountsJSON, 'utf8')
-    res.render('payment', { message: 'payment successful', account: accounts.credit })
+    accounts.credit.available += parseInt(req.body.amount, 10);
+    const accountsJSON = JSON.stringify(accounts, null, 4);
+    fs.writeFileSync(path.join(__dirname, 'json', 'accounts.json'), accountsJSON, 'utf8');
+    res.render('payment', { message: 'Payment Successful', account: accounts.credit })
 })
 
 app.get('/profile', (req, res) =>{
